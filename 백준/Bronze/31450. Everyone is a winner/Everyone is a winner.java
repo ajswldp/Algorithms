d06f4796212c3344e0752c478f36301abde0d35c
ajswldp <126873964+ajswldp@users.noreply.github.com>
2024-08-23 16:25:15 +0900

    [Bronze V] Title: Everyone is a winner, Time: 168 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/31450. Everyone is a winner/Everyone is a winner.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf(sc.nextInt()%sc.nextInt()==0?"Yes":"No");
+    }
+}
