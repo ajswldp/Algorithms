0062c7cfed6b13c9390629b46a543c20e5e80f2e
ajswldp <126873964+ajswldp@users.noreply.github.com>
2024-08-23 16:17:14 +0900

    [Bronze V] Title: Metronome, Time: 180 ms, Memory: 17800 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextInt();
+        System.out.printf("%.2f", a/4.0);
+    }
+}
