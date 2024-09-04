909e12299e9b4b04adaae0dba0e67218410295f2
ajswldp <126873964+ajswldp@users.noreply.github.com>
2024-09-04 20:06:04 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.68 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,3 +1,17 @@
+/**
+ * 1. sum 선언 및 0으로 초기화
+ * 2. moves의 길이만큼 반복:
+ *    1. board의 열의 갯수만큼 반복:
+ *       1. 변수선언 및 특정 값으로 초기화
+ *       2. board의 move열에서 0제외 가장 높이 있는 값을 스택에 push
+ *       3. board에서 값을 꺼낸 곳을 0으로 초기화
+ *    2. 만약 스택이 비어있지 않고, 스택의 최상단의 값이 board에서 꺼낸 값과 같다면:
+ *       1. pop
+ *       2. sum++
+ *    3. 변수가 초기값이 아니라면(board에서 값을 꺼냈으면) 스택에 push
+ *    
+ *    return sum<<1;
+ */
 import java.util.*;
 class Solution {
     public int solution(int[][] board, int[] moves) {
@@ -20,7 +34,6 @@ class Solution {
             else{
                 stack.push(num);
             }
-            //System.out.println(stack.toString());
         }
         return sum<<1;
     }
