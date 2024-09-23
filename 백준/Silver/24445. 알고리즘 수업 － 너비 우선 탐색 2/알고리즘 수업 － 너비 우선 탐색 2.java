42b4f78a37d8cc6542bf252b29d21b8d73bbfc47
ajswldp <126873964+ajswldp@users.noreply.github.com>
2024-09-23 20:06:12 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 1676 ms, Memory: 326924 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -0,0 +1,46 @@
+
+import java.util.*;
+public class Main {
+    static int[] arr;
+    static int cont = 1;
+    static Queue<Integer> q;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int N = sc.nextInt();
+        int s = sc.nextInt()-1;
+        arr = new int[n];
+        List<List<Integer>> board = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            board.add(new ArrayList<>());
+        }
+        for(int i = 0; i < N; i++) {
+            int a = sc.nextInt()-1;
+            int b = sc.nextInt()-1;
+            board.get(a).add(b);
+            board.get(b).add(a);
+        }
+        for(int i = 0; i < n; i++) {
+            Collections.sort(board.get(i), Collections.reverseOrder());
+        }
+        arr[s] = cont++;
+        q = new LinkedList<>();
+        q.add(s);
+        while(!q.isEmpty()) {
+            f(board);
+        }
+        System.out.println(Arrays.toString(arr)
+                .replace("[", "")
+                .replace("]", "")
+                .replace(", ", " "));
+    }
+    public static void f(List<List<Integer>> board){
+        int n = q.poll();
+        for(int i:board.get(n)){
+            if(arr[i]==0){
+                q.add(i);
+                arr[i] = cont++;
+            }
+        }
+    }
+}
