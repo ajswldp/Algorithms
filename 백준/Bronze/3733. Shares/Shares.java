d2dd4738f3d5195f1eb7d20379e098ef1689c7d3
ajswldp <126873964+ajswldp@users.noreply.github.com>
2024-09-26 08:54:13 +0900

    [Bronze V] Title: Shares, Time: 168 ms, Memory: 17464 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/Shares.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+import java.util.*;
+class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int a = sc.nextInt()+1;
+            System.out.println(sc.nextInt()/a);
+        }
+    }
+}
