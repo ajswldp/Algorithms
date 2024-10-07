02bc8217a5b61366c8bdf17ec71d7d49f826e1ca
ajswldp <126873964+ajswldp@users.noreply.github.com>
2024-10-07 19:12:35 +0900

    [Bronze IV] Title: Sounds fishy!, Time: 164 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/6764. Sounds fishy！/Sounds fishy！.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        if(a>b && b>c && c>d)
+            System.out.println("Fish Diving");
+        else if(a<b && b<c && c<d)
+            System.out.println("Fish Rising");
+        else if(a == b && a == c && a == d)
+            System.out.println("Fish At Constant Depth");
+        else
+            System.out.println("No Fish");
+    }
+}
