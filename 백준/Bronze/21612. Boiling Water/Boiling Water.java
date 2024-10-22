886e880ae939faf8ae1e2c698416e37854d61582
ajswldp <126873964+ajswldp@users.noreply.github.com>
2024-10-22 09:14:32 +0900

    [Bronze IV] Title: Boiling Water, Time: 168 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/21612. Boiling Water/Boiling Water.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(5*n-400);
+        System.out.println(Integer.compare(100, n));
+    }
+}
