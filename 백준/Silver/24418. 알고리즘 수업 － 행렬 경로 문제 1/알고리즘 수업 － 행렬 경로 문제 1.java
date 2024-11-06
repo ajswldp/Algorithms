4f1b6dc368953a18f9d92e58aa781977b5696e76
ajswldp <126873964+ajswldp@users.noreply.github.com>
2024-11-06 18:53:32 +0900

    [Silver IV] Title: 알고리즘 수업 - 행렬 경로 문제 1, Time: 188 ms, Memory: 18436 KB -BaekjoonHub

---
+++ 백준/Silver/24418. 알고리즘 수업 － 행렬 경로 문제 1/알고리즘 수업 － 행렬 경로 문제 1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n+2][n+2];
+        arr[1][0] = 1;
+        for(int i = 1;i<=n+1;i++){
+            for(int j = 1;j<=n+1;j++){
+                arr[i][j] = arr[i-1][j]+arr[i][j-1];
+            }
+        }
+        System.out.println(arr[n+1][n+1]+" "+(n*n));
+    }
+}
