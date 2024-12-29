3f6705907a59fb2b49a0ffa0f2e8da7b872e7441
ajswldp <126873964+ajswldp@users.noreply.github.com>
2024-12-29 23:24:41 +0900

    [Gold I] Title: 구간 합 구하기, Time: 2016 ms, Memory: 346188 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,4 +1,3 @@
-
 import java.io.*;
 import java.math.BigInteger;
 import java.util.*;
@@ -7,41 +6,39 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt(), b = sc.nextInt() + sc.nextInt();
-        BigInteger[] arr = new BigInteger[a+1];
-        BigInteger[] tree = new BigInteger[a+1];
+        long[] arr = new long[a+1];
+        long[] tree = new long[a+1];
         for(int i = 1; i <= a; i++) {
-            arr[i] = sc.nextBigInteger();
-            tree[i] = BigInteger.ZERO;
+            arr[i] = sc.nextLong();
         }
         for(int i = 1; i <= a; i++) {
-            tree[i] = arr[i].add(tree[i]);
+            tree[i] += arr[i];
             int p = i+(i&-i);
             if(p<=a){
-                tree[p] = tree[p].add(tree[i]);
+                tree[p] += tree[i];
             }
         }
         for(int i = 0; i < b; i++) {
             boolean bool = sc.nextInt() == 1;
             if(bool) {
-                //바꾸기
-                int idx = sc.nextInt();//능딸이라 구현 못하는중
-                BigInteger input = sc.nextBigInteger();
-                BigInteger sub = input.subtract(arr[idx]);
+                int idx = sc.nextInt();
+                long input = sc.nextLong();
+                long sub = input - arr[idx];
                 arr[idx] = input;
                 while(idx<=a){
-                    tree[idx] = tree[idx].add(sub);
+                    tree[idx] += sub;
                     idx+=idx&(-idx);
                 }
             }
             else{
                 int s = sc.nextInt()-1, e = sc.nextInt();
-                BigInteger sum = BigInteger.ZERO;
+                long sum = 0;
                 while(e>0){
-                    sum = sum.add(tree[e]);
+                    sum += tree[e];
                     e -= e&(-e);
                 }
                 while(s>0){
-                    sum = sum.subtract(tree[s]);
+                    sum = sum-tree[s];
                     s -= s&(-s);
                 }
                 System.out.println(sum);
