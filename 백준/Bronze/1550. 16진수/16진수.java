6e81755194b86b613a304a096ac348f9a8165120
ajswldp <126873964+ajswldp@users.noreply.github.com>
2025-02-06 14:05:22 +0900

    [Bronze II] Title: 16진수, Time: 176 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws RuntimeException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Integer.parseInt(sc.nextLine(), 16));
+    }
+}
