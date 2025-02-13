6d5c87610c0eece84c82d373fa2057b03ce400ef
ajswldp <126873964+ajswldp@users.noreply.github.com>
2025-02-13 12:52:17 +0900

    [Gold V] Title: 트리, Time: 176 ms, Memory: 17788 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -1,33 +1,60 @@
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
 
-class Main{
+class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<List<Integer>> list = new ArrayList<>();
         int n = sc.nextInt();
-        for(int i = 0; i < n; i++){
-            list.add(new ArrayList<>());
+        Tree tree = new Tree(n);
+        for(int i = 0; i < n; i++) {
+            tree.link(i, sc.nextInt());
         }
-        int r = 0;
-        for(int i = 0; i < n; i++){
-            int a = sc.nextInt();
-            if(a==-1) r = i;
-            else list.get(a).add(i);
-        }
-        int d = sc.nextInt();
-        list.set(d, new ArrayList<>());
-        list.forEach(a-> a.removeIf(b->b==d));
-        if(r==d){
-            System.out.println(0);
-            return;
-        }
-        System.out.println(f(list, r));
+        tree.remove(sc.nextInt());
+        System.out.println(tree.leaf());
     }
-    public static int f(List<List<Integer>> list, int r){
-        AtomicInteger num = new AtomicInteger();
-        if(list.get(r).isEmpty()) return 1;
-        list.get(r).forEach(a-> num.addAndGet(f(list, a)));
-        return num.get();
+    public static class Tree{
+        List<Node> nodeList = new ArrayList<>();
+        Node root;
+        Tree(int n){
+            for(int i = 0;i < n;i++) {
+                nodeList.add(new Node());
+            }
+        }
+        void link(int child, int parent) {
+            if(parent == -1){
+                root = nodeList.get(child);
+            }
+            else {
+                nodeList.get(child).parent = nodeList.get(parent);
+                nodeList.get(parent).children.add(nodeList.get(child));
+            }
+        }
+        void remove(int node) {
+            if(nodeList.get(node).parent==null) {
+                root = null;
+            }
+            else{
+                nodeList.get(node).parent.children.remove(nodeList.get(node));
+            }
+        }
+        int leaf(){
+            int count = 0;
+            if(root!=null){
+                List<Node> bfsNodes = new ArrayList<>();
+                bfsNodes.add(root);
+                while(!bfsNodes.isEmpty()){
+                    Node node = bfsNodes.remove(0);
+                    if(node.children.isEmpty())count++;
+                    bfsNodes.addAll(node.children);
+                }
+            }
+            return count;
+        }
+        public static class Node{
+            Node parent;
+            List<Node> children;
+            Node(){
+                children = new ArrayList<>();
+            }
+        }
     }
 }
