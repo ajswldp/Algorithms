322e9de482653fa472542f3fed72a4aa905cee6f
ajswldp <126873964+ajswldp@users.noreply.github.com>
2025-03-03 04:35:07 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.c
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.c
@@ -1,10 +1,2 @@
 #include <stdio.h>
-int main(){
-    int n;
-    scanf("%d", &n);
-    int count = 0;
-    for(int i = 5;i<=n;i*=5){
-        count +=n/i;
-    }
-    printf("%d", count);
-}
+int main(){int n, count = 0;scanf("%d", &n);for(int i=5;i<=n;i*=5)count+=n/i;printf("%d", count);}
