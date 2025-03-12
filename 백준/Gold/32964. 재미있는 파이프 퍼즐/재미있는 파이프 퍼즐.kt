691e3f396cf6e52995e74d0fa921d4032faa3f86
ajswldp <126873964+ajswldp@users.noreply.github.com>
2025-03-12 14:10:16 +0900

    [Gold V] Title: 재미있는 파이프 퍼즐, Time: 296 ms, Memory: 22572 KB, Score: 13 point -BaekjoonHub

---
+++ 백준/Gold/32964. 재미있는 파이프 퍼즐/재미있는 파이프 퍼즐.kt
@@ -0,0 +1,38 @@
+import java.util.*
+fun f1(sTop: Boolean, eTop: Boolean, s1: String, s2: String):Boolean {
+    var top = 0
+    var output = true
+    var sTop2 = sTop
+    for(i in s1.indices){
+        if(s1[i] != s2[i]){
+            output = output && f(sTop2, s1[i]=='I', s1.substring(top, i), s2.substring(top, i))
+            sTop2 = s1[i]=='I'
+            top = i+1
+        }
+    }
+    return output&& f(sTop2, eTop, s1.substring(top), s2.substring(top))
+}
+fun f(sTop:Boolean, eTop:Boolean, s1:String, s2:String):Boolean {
+    var isTop = sTop
+    for(i in s1.indices){
+        if(s1[i] == 'L')isTop = !isTop
+    }
+    return sTop == eTop
+}
+fun main(args: Array<String>) = with(Scanner(System.`in`)) {
+    nextLine()
+    val s1 = nextLine()
+    val s2 = nextLine()
+    var output = f1(true, false, s1.substring(1, s1.length-1), s2.substring(1, s2.length-1))
+    if(s2[0] == 'L')output = output || f1(false, false, s1.substring(1, s1.length-1), s2.substring(1, s2.length-1))
+    if(s1[s1.length-1] == 'L'){
+        if(s2[0] == 'L')output = output || f1(false, true, s1.substring(1, s1.length-1), s2.substring(1, s2.length-1))
+        output = output || f1(true, true, s1.substring(1, s1.length-1), s2.substring(1, s2.length-1))
+    }
+    if(output){
+        println("YES")
+    }
+    else{
+        println("NO")
+    }
+}
