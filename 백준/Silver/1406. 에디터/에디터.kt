07b0b035cb4441dcdbc06828e31c2c13ba0e34a2
ajswldp <126873964+ajswldp@users.noreply.github.com>
2025-03-27 11:32:26 +0900

    [Silver II] Title: 에디터, Time: 372 ms, Memory: 89468 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.kt
+++ 백준/Silver/1406. 에디터/에디터.kt
@@ -1,29 +1,93 @@
-import java.io.*
-import java.util.*
 
-fun main(args: Array<String>) = with(BufferedReader(InputStreamReader(System.`in`))) {
-    val s = readLine()
-    val n = readLine().toInt()
-    val s1 = Stack<Char>()
-    val s2 = Stack<Char>()
-    for(c in s.toCharArray()){
-        s1.push(c)
+import java.io.*
+
+fun main()  = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val list = MyLinkedList<Char>()
+    val arr = readLine().toCharArray()
+    for(c in arr){
+        list.insert(c)
     }
+    val n = readLine().toInt()
     for(i in 0 until n){
         val input = readLine()
         when(input[0]){
-            'P' -> s1.push(input[2])
-            'B' -> if(s1.isNotEmpty()) s1.pop()
-            'L' -> if(s1.isNotEmpty()) s2.push(s1.pop())
-            'D' -> if(s2.isNotEmpty()) s1.push(s2.pop())
+            'P' -> list.insert(input[2])
+            'L' -> list.l()
+            'D' -> list.d()
+            'B' -> list.delete()
         }
     }
-    while(s1.isNotEmpty()){
-        s2.push(s1.pop())
+    println(list.join(""))
+}
+class MyLinkedList<T>{
+
+    var head:Node<T>? = null
+    var size = 0
+    var cursor:Node<T>? = null
+
+    fun insert(value:T){
+        if(head==null){
+            head = Node(value)
+            cursor = head
+        }
+        else if(cursor==null){
+            cursor = Node(value)
+            cursor!!.r = head
+            head!!.l = cursor
+            head = cursor
+        }
+        else {
+            val node = Node(value)
+            node.l = cursor
+            node.r = cursor!!.r
+            cursor!!.r = node
+            node.r?.l = node
+            cursor = node
+        }
     }
-    val sb = StringBuilder()
-    while(s2.isNotEmpty()){
-        sb.append(s2.pop())
+    fun delete(){
+        if(cursor==null){
+            return
+        }
+        if(cursor == head){
+            head = head!!.r
+            head?.l = null
+            cursor = null
+        }
+        else{
+            cursor!!.l!!.r = cursor!!.r
+            cursor!!.r?.l = cursor!!.l
+            cursor = cursor!!.l
+        }
     }
-    println(sb.toString())
+    fun l(){
+        cursor = cursor?.l
+    }
+    fun d(){
+        if(cursor==null){
+            cursor = head
+        }
+        else if(cursor!!.r!=null){
+            cursor = cursor!!.r
+        }
+    }
+
+    fun join(s:String): String {
+        if(head==null){
+            return s
+        }
+        val sb = StringBuilder(head!!.data.toString())
+        var node = head!!.r
+        while(node != null){
+            sb.append(s)
+            sb.append(node.data.toString())
+            node = node.r
+        }
+        return sb.toString()
+    }
+
+    class Node<T>(val data: T){
+        var l:Node<T>? = null
+        var r:Node<T>? = null
+    }
 }
