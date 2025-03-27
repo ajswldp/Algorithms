6ae2fb1deb1698708c764ecbb550adfbcfc2d5da
ajswldp <126873964+ajswldp@users.noreply.github.com>
2025-03-27 19:53:25 +0900

    [Bronze II] Title: Hashing, Time: 76 ms, Memory: 11936 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.kt
+++ 백준/Bronze/15829. Hashing/Hashing.kt
@@ -1,12 +1,12 @@
+
 import java.io.*
-import kotlin.math.pow
 
 fun main()  = with(BufferedReader(InputStreamReader(System.`in`))) {
     readLine()
     val input = readLine()
     var sum = 0L
-    for(i in input.indices){
-        sum = (sum+(input[i].code-'a'.code+1)* 31.0.pow(i.toDouble()).toLong())%1234567891
+    for(i in 1 .. input.length){
+        sum = (input[input.length-i].code-'a'.code+1+ sum*31)%1234567891
     }
     print(sum)
 }
