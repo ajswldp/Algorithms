18f4d238cf935bcd5afde50a670279fa909f8569
ajswldp <126873964+ajswldp@users.noreply.github.com>
2025-09-14 04:40:00 +0900

    [level 3] Title: N으로 표현, Time: 3602.63 ms, Memory: 377 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.kt
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.kt
@@ -1,33 +1,6 @@
 class Solution {
     fun solution(N: Int, number: Int): Int {
-        var max = N.toLong()
-        val map = mutableMapOf<Long, Int>()
-        var q = mutableListOf<Long>()
-        for(i in 1..8){
-            add(max, i, map, q)
-            max *= 10
-            max += N
-        }
-        for(i in 1..8){
-            while(q.isNotEmpty()){
-                val x = q.removeFirst()
-                for(num in map.keys.toList()){
-                    if(map[num]!! + map[x]!! <= 8 && num != 0L && x != 0L){
-                        add(num + x, map[num]!! + map[x]!!, map, q)
-                        add(num - x, map[num]!! + map[x]!!, map, q)
-                        add(x - num, map[num]!! + map[x]!!, map, q)
-                        add(num * x, map[num]!! + map[x]!!, map, q)
-                        add(num / x, map[num]!! + map[x]!!, map, q)
-                        add(x / num, map[num]!! + map[x]!!, map, q)
-                    }
-                }
-            }
-        }
-        return map[number.toLong()] ?: -1
-    }
-    fun add(num1:Long, num2:Int, map: MutableMap<Long, Int>, q: MutableList<Long>){
-        if((map[num1] ?: 10) <= num2)return
-        map[num1] = num2
-        q.add(num1)
+        var answer = 0
+        return answer
     }
 }
